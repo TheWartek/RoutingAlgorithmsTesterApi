324b9a0302fc9ef5d361ca820dbb98eca4b9599e
TheWartek <grief888@gmail.com>
2012-05-17 18:39:41 +0200

    Dodanie metody equals oraz konstruktora tworzacego krawedz bez wagi

--- src/pl/mgrproject/api/Edge.java
+++ src/pl/mgrproject/api/Edge.java
@@ -1,15 +1,39 @@
 package pl.mgrproject.api;
 
 public class Edge<T> {
-    
+
     public int first;
     public int last;
     public T value;
-    
+
     public Edge(int first, int last, T value) {
-	this.first = first;
-	this.last = last;
+	this(first, last);
 	this.value = value;
     }
-    
+
+    public Edge(int first, int last) {
+	this.first = first;
+	this.last = last;
+	this.value = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+	if (!(o instanceof Edge)) {
+	    return false;
+	}
+	Edge e = (Edge) o;
+	if (value != null) {
+	    if (e.first != first || e.last != last || e.value != value) {
+		return false;
+	    }
+	} else {
+	    if (e.first != first || e.last != last) {
+		return false;
+	    }
+	}
+
+	return true;
+    }
+
 }
