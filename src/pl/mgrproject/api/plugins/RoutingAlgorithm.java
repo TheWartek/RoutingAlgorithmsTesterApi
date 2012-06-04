eacf5bcf24c4c294179866eb4adecb6f5862be3c
TheWartek <grief888@gmail.com>
2012-06-04 21:29:17 +0200

    Zmiana interfejsu. Algorytm Dijkstry informacje o wierzcholku docelowym potrzebuje dopiero po przetworzeniu grafu.

--- src/pl/mgrproject/api/plugins/RoutingAlgorithm.java
+++ src/pl/mgrproject/api/plugins/RoutingAlgorithm.java
@@ -7,6 +7,6 @@ import pl.mgrproject.api.Graph;
 
 public interface RoutingAlgorithm extends Plugin {
     public void setGraph(Graph<?> g);
-    public List<Point> getPath();
-    public void run(int start, int stop);
+    public List<Point> getPath(int stop);
+    public void run(int start);
 }
